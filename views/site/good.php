534f35945199f6e785e5db5de1992a952b88d685
tony43400141 <huamanlou@b5m.com>
2015-03-05 11:36:31 +0800

    yii添加site测试

---
+++ views/site/good.php
@@ -0,0 +1,14 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+$this->title = 'Good';
+?>
+<div class="site-about">
+    <h1>我的天呀</h1>
+
+    <p>
+        This is the About page. You may modify the following file to customize its content:
+    </p>
+
+	</div>
